033f07a7fce852545eb61bbd8e7f33538eb31576
Lorenzo Fontana <lorenzo.fontana@facile.it>
2015-07-22 12:01:32 +0200

    Header

--- src/Module.php
+++ src/Module.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Sentry Module
+ *
+ * @link      http://github.com/facile-it/sentry-module for the canonical source repository
+ * @copyright Copyright (c) 2015 Facile.it
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License Version 2.0
+ */
 
 namespace Facile\SentryModule;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Sentry Module
  *
